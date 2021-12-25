5f7159b1e66be8b87fbb4bd51fbb1c096e92a811
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-12-25 17:47:39 +0100

    make sketches of _crreate and _remove

--- Tree.c
+++ Tree.c
@@ -14,6 +14,8 @@
 #include "path_utils.h"
 #include "Tree.h"
 
+#define ROOT_PATH "/"
+
 struct Tree {
   char* dir_name;
   HashMap* subdirs;
@@ -38,7 +40,7 @@ Tree* new_dir(char* name)
 
 Tree* tree_new()
 {
-  char* name = strdup("/");
+  char* name = strdup(ROOT_PATH);
   return new_dir(name);
 }
 
@@ -65,6 +67,7 @@ Tree* find_dir(Tree* root, const char* path)
   char component[MAX_FOLDER_NAME_LENGTH + 1];
   const char* subpath = path;
   assert(is_path_valid(path));
+
   while ((subpath = split_path(subpath, component))) {
     if (!root)
       break;
@@ -75,29 +78,31 @@ Tree* find_dir(Tree* root, const char* path)
   return root;
 }
 
-/* Find a directory and its parent.
- * return val: TODO! what do we want it to return? i want to allow *dir == NULL
- * but i want *parent->dir_name to be precisely equal to the penultimate comp! */
-bool find_parent_dir(Tree* root, Tree** parent, Tree** dir, const char* path)
+/**
+ * Find a directory specified by a path. The directory will be saved under `dir`
+ * and its parent will be saved under `parent`. In case the `dir` does not exist
+ * then it will be set to `NULL`. In case even the parent directory does not
+ * exist then it will be set to `NULL` as well.
+ */
+void find_parent_dir(Tree* root, Tree** parent, Tree** dir, const char* path)
 {
   char component[MAX_FOLDER_NAME_LENGTH + 1];
   const char* subpath = path;
-  assert(is_path_valid(path));
   *parent = NULL;
   *dir = root;
+
   while ((subpath = split_path(subpath, component))) {
-    /* this is not correct */
-    if (!root)
-      return false;
+    /* the path has not yet ended yet we have run out of directories to visit */
+    if (!root) {
+      *parent = *dir;
+      return;
+    }
 
     root = hmap_get(root->subdirs, component);
     *parent = *dir;
     *dir = root;
   }
-
-  return true;
 }
-
 
 char* tree_list(Tree* tree, const char* path)
 {
@@ -109,23 +114,107 @@ char* tree_list(Tree* tree, const char* path)
   return make_map_contents_string(dir->subdirs);
 }
 
+/* VERSION OF CREATE NOT USING PATH_UTILS: */
+void get_last_component(const char* path, char* last);
+
 /* TODO -- i want to find the parent directory, then create a new dir with its
  * name being the last component from the path and add it to the hashmap.
  * how to devise the function for finding the parent then? split path but
  * keeping the last component... huh. */
-int tree_create(Tree* tree, const char* path)
-{  
+int tree_create_pdir(Tree* tree, const char* path)
+{
   Tree* parent;
   Tree* dir;
+  Tree* subdir;
+  char* last_component;
+
+  if (!is_path_valid(path))
+    return EINVAL;
+
+  find_parent_dir(tree, &parent, &dir, path);
 
   /* The parent to the new directory does not exist. */
-  if (!find_parent_dir(tree, &parent, &dir, path))
+  if (!parent)
     return ENOENT;
 
-  /* The new directory already exists. */
+  /* The directory already exists. */
   if (dir)
     return EEXIST;
 
-  
-  return 1;
+  get_last_component(path, last_component);
+  subdir = new_dir(last_component);
+  hmap_insert(parent->subdirs, last_component, subdir);
+
+  return 0;
 }
+
+/* VERSION OF CREATE THAT INDEED USES PATH_UTILS.H */
+int tree_create(Tree* tree, const char* path)
+{
+  Tree* parent;
+  Tree* subdir;
+  char* parent_path;
+  char last_component[MAX_FOLDER_NAME_LENGTH + 1];
+  char* subdir_name;
+
+  if (!is_path_valid(path))
+    return EINVAL;
+
+  parent_path = make_path_to_parent(path, last_component);
+
+  /* Create called on "/" -- the root already exists obviously */
+  if (!parent_path)
+    return EEXIST;
+
+  parent = find_dir(tree, parent_path);
+  free(parent_path);
+
+  /* The parent does not exist. */
+  if (!parent)
+    return ENOENT;
+
+  /* The subdir we want to create already exists. */
+  if (hmap_get(parent->subdirs, last_component))
+    return EEXIST;
+
+  subdir_name = strdup(last_component);
+  subdir = new_dir(subdir_name);
+
+  /* add the newly created subdirectory as a parent's child */
+  hmap_insert(parent->subdirs, subdir_name, new_dir);
+  return 0;
+}
+
+/* we find the parent, inside we find the child, check if it is empty, remove
+ * the dir from the map and then from the memory. */
+int tree_remove(Tree* tree, const char* path)
+{
+  Tree* parent;
+  Tree* dir;
+  Tree* subdir;
+  char* parent_path;
+  char last_component[MAX_FOLDER_NAME_LENGTH + 1];
+  
+  if (!is_path_valid(path))
+    return EINVAL;
+  else if (strcmp(path, ROOT_PATH))
+    return EBUSY;
+
+  parent_path = make_path_to_parent(path, last_component);
+  parent = find_dir(tree, parent_path);
+  free(parent_path);
+
+  subdir = hmap_get(parent->subdirs, last_component);
+  if (!subdir)
+    return ENOENT;
+
+  if (hmap_size(subdir->subdirs) > 0)
+    return ENOTEMPTY;
+
+  hmap_remove(parent->subdirs, last_component);
+  tree_free(subdir);
+  
+  return 0;
+}
+
+int tree_move(Tree* tree, const char* source, const char* target);
