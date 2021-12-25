05935707e368d8d5862d626e0c49f1eb8e8b390f
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-12-25 19:09:13 +0100

    cool tests

--- main.c
+++ main.c
@@ -1,30 +1,12 @@
-#include "HashMap.h"
-#include "Tree.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-void print_map(HashMap* map)
-{
-  const char* key = NULL;
-  void* value = NULL;
-  printf("Size=%zd\n", hmap_size(map));
-  HashMapIterator it = hmap_iterator(map);
-
-  while (hmap_next(map, &it, &key, &value)) {
-    printf("Key=%s Value=%p\n", key, value);
-  }
-
-  printf("\n");
-}
-
+#include "Tree.h"
 
 void simple_tree_test()
 {
-  printf("SImple tree test\n");
+  printf("SIMPLE TREE TEST\n");
   Tree* root = tree_new();
   int e1 = tree_create(root, "/a/");
   int e2 = tree_create(root, "/b/");
@@ -33,34 +15,44 @@ void simple_tree_test()
     printf("e1 = %d, e2 = %d\n", e1, e2);
 
   char* listing = tree_list(root, "/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
   free(listing);
   int e3 = tree_create(root, "/a/b/");
   int e4 = tree_create(root, "/a/c/");
+  int e5 = tree_create(root, "/a/wyjebany/");
 
   listing = tree_list(root, "/a/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
   free(listing);
 
-  int e5 = tree_move(root, "/a/", "/b/chuuj/");
+  int e6 = tree_move(root, "/a/", "/b/chuuj/");
 
-  if (e3 || e4 || e5)
+  if (e3 || e4 || e5 || e6)
     printf("e3 = %d, e4 = %d, e5 = %d\n", e3, e4, e5);
 
   listing = tree_list(root, "/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
   free(listing);
 
   listing = tree_list(root, "/a/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
   free(listing);
 
   listing = tree_list(root, "/b/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
   free(listing);
 
   listing = tree_list(root, "/b/chuuj/");
-  printf("listing: %s\n", listing);
+  printf("\t%s\n", listing);
+  free(listing);
+
+  int e7 = tree_remove(root, "/b/chuuj/wyjebany/");
+
+  if (e7)
+    printf("e7 = %d\n", e7);
+
+  listing = tree_list(root, "/b/chuuj/");
+  printf("\t%s\n", listing);
   free(listing);
 
   tree_free(root);
@@ -68,18 +60,6 @@ void simple_tree_test()
 
 int main(void)
 {
-  HashMap* map = hmap_new();
-  hmap_insert(map, "a", hmap_new());
-  print_map(map);
-
-  HashMap* child = (HashMap*)hmap_get(map, "a");
-  hmap_free(child);
-  hmap_remove(map, "a");
-  print_map(map);
-
-  hmap_free(map);
-
   simple_tree_test();
-
   return 0;
 }
