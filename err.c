080655eaf4643718afe4534e01ac8617d832ef98
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-12-28 17:05:21 +0100

    reformat the utils

--- err.c
+++ err.c
@@ -1,36 +1,38 @@
-#include "err.h"
-#include <errno.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <string.h>
+#include "err.h"
 
-void syserr(const char* fmt, ...)
+extern const int sys_nerr;
+
+void syserr(int bl, const char* fmt, ...)
 {
-    va_list fmt_args;
+  va_list fmt_args;
 
-    fprintf(stderr, "ERROR: ");
+  fprintf(stderr, "ERROR: ");
 
-    va_start(fmt_args, fmt);
-    vfprintf(stderr, fmt, fmt_args);
-    va_end(fmt_args);
-    fprintf(stderr, " (%d; %s)\n", errno, strerror(errno));
-    exit(1);
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end (fmt_args);
+  fprintf(stderr, " (%d; %s)\n", bl, strerror(bl));
+  exit(1);
 }
 
 void fatal(const char* fmt, ...)
 {
-    va_list fmt_args;
+  va_list fmt_args;
 
-    fprintf(stderr, "ERROR: ");
+  fprintf(stderr, "ERROR: ");
 
-    va_start(fmt_args, fmt);
-    vfprintf(stderr, fmt, fmt_args);
-    va_end(fmt_args);
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end (fmt_args);
 
-    fprintf(stderr, "\n");
-    exit(1);
+  fprintf(stderr, "\n");
+  exit(1);
 }
