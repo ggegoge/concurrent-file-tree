2c55a07a820c23175bf2b67d1e2a27c29b4c3a6d
Grzegorz Cichosz <grzes.cichosz@gmail.com>
2021-12-31 18:31:23 +0100

    idk why it was included in the file i got at the lab??

--- err.c
+++ err.c
@@ -3,12 +3,8 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
 #include "err.h"
-
-extern const int sys_nerr;
 
 void syserr(int e, const char* fmt, ...)
 {
@@ -16,26 +12,12 @@ void syserr(int e, const char* fmt, ...)
 
   if (!e)
     return;
-  
+
   fprintf(stderr, "ERROR: ");
 
   va_start(fmt_args, fmt);
   vfprintf(stderr, fmt, fmt_args);
   va_end (fmt_args);
   fprintf(stderr, " (%d; %s)\n", e, strerror(e));
-  exit(1);
-}
-
-void fatal(const char* fmt, ...)
-{
-  va_list fmt_args;
-
-  fprintf(stderr, "ERROR: ");
-
-  va_start(fmt_args, fmt);
-  vfprintf(stderr, fmt, fmt_args);
-  va_end (fmt_args);
-
-  fprintf(stderr, "\n");
   exit(1);
 }
--- err.h
+++ err.h
@@ -4,7 +4,4 @@
 /** Print system call error message and terminate. If `e` is zero do nothing. */
 extern void syserr(int e, const char* fmt, ...);
 
-/** Print error message and terminate. */
-extern void fatal(const char* fmt, ...);
-
 #endif  /* _ERR_H_ */
